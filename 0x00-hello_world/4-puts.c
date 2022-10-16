7536a0ef0e2dc41dfce43c9b2984c298e09234b9
prevail-dot <esehgodprevail@gmail.com>
2022-10-16 08:29:34 -0700

    what a ride

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'Programming is like building a multilingual  puzzle'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
