1d2b8039556814c3cf6d2c0e0978a19254914e07
prevail-dot <esehgodprevail@gmail.com>
2022-10-18 15:42:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:  I sometimes suffer from insomnia;
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a', x <= 'z', x++)
+	{
+		putchar(x);
+	}
+
+	return (0);
+}
+
