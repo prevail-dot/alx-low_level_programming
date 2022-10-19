9da3d87cfd597d44aa5d8361fdfc764413d501d7
prevail-dot <esehgodprevail@gmail.com>
2022-10-19 10:03:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphABET;
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z', 'Z'; a++)
+	{
+		putchar(x);
+	}
+	{
+		putchar(toupper(x));
+	}
+	{
+		putchar('\n');
+	}
+
+	return (0);
+}
