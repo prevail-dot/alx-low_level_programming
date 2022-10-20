30233d634961978b818070719833297fffe32515
prevail-dot <esehgodprevail@gmail.com>
2022-10-20 00:54:35 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include  <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Numbers;
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		printf("%d \n", num);
+	}
+
+	return (0);
+}
