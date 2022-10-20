5f864217ea825b0f49631f515c4076e760dad7db
prevail-dot <esehgodprevail@gmail.com>
2022-10-20 00:14:26 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: When I was having that alphabet soup;
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int = x
+
+	for (x = 'a'; x <= 'z' x != 'e' x != 'q'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
